7aa52f4ccecec9a0ca4e46bea7d3e64e59eda57e
nikita <nikitastocihev12@gmail.com>
2026-03-21 12:28:29 +0100

    first_homework.py

---
+++ first_homework.py
@@ -0,0 +1,47 @@
+my_favourite_city =  'https://de.wikipedia.org/wiki/Saarbr%C3%BCcken'
+print('My Favourite City is ',my_favourite_city)
+  my_accosiation = 'https://de.wikipedia.org/wiki/Meer'
+  print('My Accosiation is ',my_accosiation)
+  my_city_accosiation = '🦋'
+  print(My second accosiation is ',my_city_accosiation')
+
+ city_name ='Saarbruecken'
+ print('This city name is ',city_name)
+
+ unikod_umbrella_code = 'U+2602'
+ print('Unikod Umbrella code is ',unikod_umbrella_code)
+
+ unikod_umbrella_emoji = '☂'
+ print('Unikod Umbrella emoji is ',unikod_umbrella_emoji)
+
+
+
+
+
+eneid_poem =
+ЧАСТИНА ПЕРША
+
+1
+
+Еней був парубок моторний
+
+І хлопець хоть куди козак,
+
+Удавсь на всеє зле проворний,
+
+Завзятіший од всіх бурлак.
+
+Но греки, як спаливши Трою,
+
+Зробили з неї скирту гною,
+
+Він, взявши торбу, тягу дав;
+
+Забравши деяких троянців,
+
+Осмалених, як гиря, ланців,
+
+П’ятами з Трої накивав.
+
+print(eneid_poem)
+
