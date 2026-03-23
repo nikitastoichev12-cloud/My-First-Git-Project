bcffbdd719b17406afc352120e255318d31d1361
nikita <nikitastocihev12@gmail.com>
2026-03-23 19:23:21 +0100

    strings homework

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello")
+import poems
+import sign
+
+author_name = "Твоє ім'я"
+
+print(sign.SIGN_TEMPLATE.format(author_name))
+
+wishes = "Більше практики\nМенше сухої теорії"
+print(wishes)
+
+stars = "**************************************************************************"
+
+print(stars)
+print(poems.POEM_1)
+print(stars)
+print(poems.POEM_2)
+print(stars)
