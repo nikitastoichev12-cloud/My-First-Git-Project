c2d75c66a6ae6338d7dfb8451b99b2cc2318750c
nikita <nikitastocihev12@gmail.com>
2026-03-23 20:57:50 +0100

    strings homework

---
+++ first_homework.py
@@ -0,0 +1,40 @@
+my_favourite_city = 'https://de.wikipedia.org/wiki/Saarbr%C3%BCcken'
+print('My favourite city is', my_favourite_city)
+my_accosiation = 'https://de.wikipedia.org/wiki/Accosiation'
+print('My accosiation is', my_accosiation)
+my_city_accositation = '🌧️'
+print('My second accosiation is', my_city_accositation)
+city_name = ('Saarbruecken')
+print('This city name is:', city_name)
+unicode_rain_code = 'U+1F327'
+print('Unicode rain code is:', unicode_rain_code)
+unicode_rain_emoji = '🌧'
+print('Unicode rain emoji is:', unicode_rain_emoji)
+
+eneid_poem = """""
+
+ЧАСТИНА ПЕРША
+
+1
+
+Еней був парубок моторний
+
+І хлопець хоть куди козак,
+
+Удавсь на всеє зле проворний,
+
+Завзятіший од всіх бурлак.
+
+Но греки, як спаливши Трою,
+
+Зробили з неї скирту гною,
+
+Він, взявши торбу, тягу дав;
+
+Забравши деяких троянців,
+
+Осмалених, як гиря, ланців,
+
+П’ятами з Трої накивав.
+
+print(eneid_poem)
